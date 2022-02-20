e7bf4cd136f43eb9c3d5a6ae4f9d1e7467062aa6
Amelia tziougras <amelia@Amelias-MacBook-Pro.local>
2022-02-20 11:39:40 -0500

    version2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use(process.env.BASE_URL+'/', express.static('public'))
+app.use(express.static('public'))
 
 app.get(process.env.BASE_URL+'/api/meals/:ingredient', (req, res) => {
    
